69a1487cf317630f3f60f28e2a438c941f9d5c9a
André Tarnowsky <andre.tarnowsky@joocom.de>
2017-10-24 19:03:56 +0200

    add speed

--- src/enemy.ts
+++ src/enemy.ts
@@ -3,20 +3,21 @@ export class Enemy {
     position: number;
     node: HTMLElement;
     dead: boolean;
-
-    constructor(position: number) {
+    speed: number = 1;
+    constructor(position: number, speed: number = 1) {
         this.node = document.createElement('div');
         this.node.classList.add('enemy');
         this.node.style.left = position + 'px';
         this.dead = false;
+        this.speed = speed;
     }
- 
+
     move() {
         if (this.dead) {
             return;
         }
         let currentTop = parseInt(this.node.style.top) || 0;
-        this.node.style.top = (currentTop + 1) + 'px';
+        this.node.style.top = (currentTop + this.speed) + 'px';
     }
 
     getOffsetTop() {
