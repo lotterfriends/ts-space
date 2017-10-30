64538e19ff604b26580d390fd07725fb4d47cd91
André Tarnowsky <andre@tarnowsky.de>
2017-10-30 14:59:03 +0100

    use translate, fix keyboard control, add game over, pause explosion, and much more

--- src/game.ts
+++ src/game.ts
@@ -1,31 +1,52 @@
 import { Board } from './board';
 import { Player } from './player';
 import { Enemy } from './enemy';
+import { Infos } from './infos';
+import { Bullet } from './bullet';
 
 class Game {
     speed: number;
     player: Player;
     board: Board;
-    enemys: Array<Enemy>;
-    level: number;
+    infos: Infos;
+    enemys: Array<Enemy> = [];
+    bullets: Array<Bullet> = [];
+    level: number = 1;
+    destroyedCount: number = 0;
     startTime: Date;
     levelDurationInSec: number;
     livetime: number;
+    gameOver: boolean = false;
+    pause: boolean = false;
 
     constructor() {
-        this.level = 1;
         this.speed = 40;
-        this.board = new Board;
+        this.board = new Board();
         this.player = new Player();
+        this.infos = new Infos(this.level);
         this.board.addItem(this.player.node);
-        this.enemys = [];
         this.levelDurationInSec = 10;
     }
 
     start() {
+        document.body.appendChild(this.infos.getNode());
         document.body.appendChild(this.board.node);
         this.startTime = new Date();
+        this.initKeyboardListener();
         this.run();
+    }
+
+    initKeyboardListener():void {
+        document.addEventListener('keydown', (event) => {
+            if (event.which === 32) { // space
+                let bullet = this.player.fire();
+                this.board.addItem(bullet.getNode());
+                this.bullets.push(bullet);
+            }
+            if (event.which === 80) { // p
+                this.pause = !this.pause;
+            }
+        });
     }
 
     spawnEnemy() {
@@ -35,23 +56,72 @@ class Game {
         this.board.addItem(enemy.node);
     }
 
+    setGameOver() {
+        if (!this.gameOver) {
+            this.gameOver = true;
+            this.board.node.classList.add('gameOver');
+            var goNode = document.createElement('div');
+            goNode.classList.add('gameOverText');
+            goNode.innerHTML = 'GAME OVER!';
+            this.board.addItem(goNode);
+        }
+    }
+
     run() {
 
-        if ((Date.now() - this.startTime.getTime()) / 1000 > this.level * this.levelDurationInSec) {
-            this.level++;
-            console.log('Reached Level %s', this.level);
-        }
-      
-        if (Math.round(Math.random() * 400) <= this.level) {
-            this.spawnEnemy();
-        }
+        if (!this.gameOver && !this.pause) {
 
-        for (let enemy of this.enemys) {
-            if (enemy.getOffsetTop() > this.board.getHeight()) {
-                enemy.destroy();
-            } else {
-                enemy.move();
+            // todo: remove pause time
+            if ((Date.now() - this.startTime.getTime()) / 1000 > this.level * this.levelDurationInSec) {
+                this.level++;
+                this.infos.setLevel(this.level);
             }
+        
+            if (Math.round(Math.random() * 400) <= this.level) {
+                this.spawnEnemy();
+            }
+
+            // collision
+            for (let enemy of this.enemys) {
+                for (let bullet of this.bullets) {
+                    var rect1 = {x: enemy.getPosition().left, y: enemy.getPosition().top, width: Enemy.WIDTH, height: Enemy.HEIGHT}
+                    var rect2 = {x: bullet.getPosition().left, y: bullet.getPosition().top, width: Bullet.WIDTH, height: Bullet.HEIGHT}
+                    
+                    if (rect1.x < rect2.x + rect2.width &&
+                    rect1.x + rect1.width > rect2.x &&
+                    rect1.y < rect2.y + rect2.height &&
+                    rect1.height + rect1.y > rect2.y) {
+                        this.bullets.splice(this.bullets.indexOf(bullet), 1);
+                        this.enemys.splice(this.enemys.indexOf(enemy), 1);
+                        bullet.destroy();
+                        enemy.destroy(this.board);
+                        this.destroyedCount++;
+                        this.infos.setKillCount(this.destroyedCount); 
+                    }
+
+                }
+            }
+
+            for (let enemy of this.enemys) {
+                if (enemy.getTop() + Enemy.HEIGHT > this.board.getHeight()) {
+                    enemy.destroy();
+                    this.enemys.splice(this.enemys.indexOf(enemy), 1);
+                    this.setGameOver();
+                } else {
+                    enemy.move();
+                }
+            }
+
+            for (let bullet of this.bullets) {
+                if (bullet.getPosition().top <= 0) {
+                    bullet.destroy();
+                    this.bullets.splice(this.bullets.indexOf(bullet), 1);
+                } else {
+                    bullet.move();
+                }
+            }
+
+            this.player.move();
         }
 
         window.requestAnimationFrame(() => {
